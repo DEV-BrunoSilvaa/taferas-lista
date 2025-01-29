f22435cf2ac011370fb5b75af0df7ac402c4021d
DEV-BrunoSilvaa <brunosurf1005@gmail.com>
2025-01-29 05:04:45 -0300

    funcionalidades botoes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,84 @@
 import { useState } from 'react';
+import Todo from "./Componentes/Todo";
+import TodoForm from './Componentes/TodoForm';
+import Search from './Componentes/Searche';
 import './App.css';
 
 function App() {
 
-  const[ todos, setTodos] = useState([
+  const[ todos, setTodos ] = useState([
     {
       id: 1,
-      Text: "criar funcionalidade x no sistema",
+      Text: "Criar Funcionalidade X No Sistema",
       category:"trabalho",
       isCompleted: false,
     },
     {
       id: 2,
-      Text: "ir para academia",
-      category:"pessoal",
+      Text: "ir Para Academia",
+      category:"Pessoal",
       isCompleted: false,
     },
     {
       id: 3,
-      Text: "estudar react",
-      category:"estudos",
+      Text: "Estudar React",
+      category:"Estudos",
       isCompleted: false,
     },
   ]);
+
+
+  //funcionalidade do botao pesquisar
+  const [search, setSearch] = useState("")
+  const addTodo = (text, category) => {
+
+    const newTodo = [
+      ...todos, 
+      {
+      id: Math.floor(Math.random() * 1000),
+      text,
+      category,
+      isCompleted: false,
+    },
+  ];
+  setTodos(newTodo);
+  };
+  //remove as tarefas 
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo =>
+    todo.id !== id ? todo : null
+    );
+    setTodos(filteredTodos);
+  }
+
+
+  // confirma as tarefas feitas 
+  const completeTudo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos);
+  }
   
   return (
     <div className="app">
-    <h1>Listas de Tarefas</h1>
+      <h1>Lista De Tarefas</h1>
+      <Search search={search}/>
     <div className='todo-list'>
-      {todos.map((todo) => (
-        <div className="todo">
-          <div className='content'>
-            <p>{todo.Text}</p>
-            <p className='category'>
-              ({todo.category})
-            </p>
-          </div>
-          <div>
-            <button>Completar Tarefa</button>
-            <button> X </button>
-          </div>
-        </div>
+      {todos
+      .filter((todo) =>
+        (todo.text?.toLowerCase() || "").includes((search?.toLowerCase() || ""))
+        )
+        .map((todo) => (
+        <Todo 
+        key={todo.id} 
+        todo={todo} 
+        removeTodo={removeTodo} 
+        completeTudo={completeTudo}/>
       ))}
     </div>
+    <TodoForm addTodo={addTodo} />
     </div>
   );
 }
